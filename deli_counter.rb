23def98fd7598bd14b574d0bd948ecaccafcaf69
Ian Mayton <ian.mayton@gmail.com>
2020-07-17 18:41:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,11 @@ def line(array)
   if array.length == 0
     puts "The line is currently empty."
   else
-    array
+    string = ''
+    array.each_with_index do |person, index|
+      line_position = index + 1
+      string << "#{line_position}. #{person}"
+    end
     puts "The line is currently:" + string
   end
 end
